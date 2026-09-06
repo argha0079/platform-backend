a38dc7463d20ef6cf80da37e8a5dea66d5fc1191
Argha Nath <argha2489@gmail.com>
2026-09-06 15:05:15 +0530

    fix: remove extensionsToTreatAsEsm from jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 export default {
     testEnvironment: 'node',
     transform: {},
-    extensionsToTreatAsEsm: ['.js'],
     testMatch: ['**/__tests__/**/*.test.js'],
     collectCoverageFrom: ['src/**/*.js', '!src/index.js'],
     coverageThreshold: {
